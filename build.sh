680deae46919546017475bcd4309d3a72c19a6a2
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-11-25 01:53:43 +0100

    ci: create build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+set -eu
+
+mkdir -p run
+echo 'eula=true' > run/eula.txt
+
+./gradlew --console plain --no-daemon runServer
+
+mkdir -p build/dist
+cp run/colors.csv build/dist/
