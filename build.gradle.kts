8dc9ab1e2381f1f71876b1cd08faa87246b20749
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-11-25 01:20:53 +0100

    build: substitute placeholders in fabric mod options

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,3 +20,17 @@ java {
 loom {
   accessWidenerPath = file("src/main/resources/palette.accesswidener")
 }
+
+tasks.processResources {
+  val props = mapOf(
+    "version" to project.version,
+    "minecraft_version" to libs.versions.minecraft.get(),
+    "fabric_loader_version" to libs.versions.fabric.loader.get(),
+  )
+
+  filteringCharset = "UTF-8"
+
+  filesMatching("fabric.mod.json") {
+    expand(props)
+  }
+}
